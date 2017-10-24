1a029bc84b23f803855337bd109c9b5502a9c088
Christopher McCormack <cmccormack@users.noreply.github.com>
2017-10-23 22:54:32 -0700

    Changed process.env comparisons to check for string instead of boolean

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ var fs = require('fs');
 var express = require('express');
 var app = express();
 
-if (!process.env.DISABLE_XORIGIN) {
+if (process.env.DISABLE_XORIGIN === 'false') {
   app.use(function(req, res, next) {
     var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
     var origin = req.headers.origin || '*';
-    if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
+    if(process.env.XORIG_RESTRICT === 'false' || allowedOrigins.indexOf(origin) > -1){
          console.log(origin);
          res.setHeader('Access-Control-Allow-Origin', origin);
          res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
